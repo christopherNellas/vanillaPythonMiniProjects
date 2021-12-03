39a6844088e8269e4fd73f8b5ade59be32c2f22e
Ian Christopher Nellas <nellas.christopher@outlook.com>
2021-12-03 20:39:26 +0800

    added a random question feature/

--- quizgame.py
+++ quizgame.py
@@ -1,4 +1,18 @@
+from math import inf
+import random as r
+import itertools as i
+
 print("Welcome to my Quiz Game")
+order = []
+for orders in i.count(start=1):
+    randomNumber = r.randrange(1,5)
+    if len(order) == 4:
+        break
+    elif randomNumber in order:
+        continue
+    else:
+        order.append(randomNumber)
+print(order)
 
 playing = input("Do you want to play? ").lower()
 #print(f"{playing}")
